36aca315cc6410a7311f7aea2a6ac6351fc36c81
clydedsouza410 <clydedsouza410@gmail.com>
2025-11-23 04:55:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ resource "aws_security_group" "web_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.0.113.0/24"]
   }
 }
